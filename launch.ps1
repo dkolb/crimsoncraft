6c2f0b480fc85055bb449928d3496fe4e2139343
David Kolb <david.kolb@krinchan.com>
2020-08-23 16:04:20 -0400

    Initial commit of dev tools

---
+++ launch.ps1
@@ -0,0 +1,37 @@
+# Remove Coretto and set up JDK 14, Extra Where-Object filter is to keep idempotency.
+$path = $env:PATH
+$path = (
+    (
+        $path.Split(';') | 
+        Where-Object {$_ -ne 'C:\Program Files\Amazon Corretto\jdk1.8.0_222\bin'} |
+        Where-Object {$_ -ne 'C:\Program Files\Java\jdk-14.0.1\bin'}
+    ) + 'C:\Program Files\Java\jdk-14.0.1\bin'
+) -join ';'
+$env:PATH = $path
+$env:JAVA_HOME = 'C:\Program Files\Java\jdk-14.0.1'
+
+Push-Location -Path ".\crimsontint-dev"
+
+java -Xms10G `
+    -Xmx10G `
+    -XX:+UseG1GC `
+    -XX:+ParallelRefProcEnabled `
+    -XX:MaxGCPauseMillis=200 `
+    -XX:+UnlockExperimentalVMOptions `
+    -XX:+DisableExplicitGC `
+    -XX:+AlwaysPreTouch `
+    -XX:G1NewSizePercent=30 `
+    -XX:G1MaxNewSizePercent=40 `
+    -XX:G1HeapRegionSize=8M `
+    -XX:G1ReservePercent=20 `
+    -XX:G1HeapWastePercent=5 `
+    -XX:G1MixedGCCountTarget=4 `
+    -XX:InitiatingHeapOccupancyPercent=15 `
+    -XX:G1MixedGCLiveThresholdPercent=90 `
+    -XX:G1RSetUpdatingPauseTimePercent=5 `
+    -XX:SurvivorRatio=32 `
+    -XX:+PerfDisableSharedMem `
+    -XX:MaxTenuringThreshold=1 `
+    -jar .\1.16.2-r6.jar nogui
+
+Pop-Location
---
+++ package.ps1
@@ -0,0 +1,15 @@
+Push-Location .\crimsontint-dev
+
+$7z = "C:\Program Files\7-Zip\7z.exe"
+
+# Options
+# -r causes recursion to be the default
+# -xr0@'..\excludelist.txt'
+#   -x == exclude
+#   r0 == recursively exclude ONLY when there's a wild card in the exclusion.
+#   @'..\excludeslist.txt' == exclude using the excludes list file.  One pattern per line.
+
+# Execute command
+& $7z a ..\upgrade_package.zip '*' -r -xr0@'..\excludelist.txt'
+
+Pop-Location
